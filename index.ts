ba665ca69fb97167d8e6db0c4124615739447ed4
hugomdmg <hugomdmg@gmail.com>
2025-01-01 16:01:53 +0000

    mongo

--- index.ts
+++ index.ts
@@ -1,13 +1,21 @@
 import express, { Request, Response } from 'express';
+import db from './src/services/data_base';
 
 const app = express();
 const PORT = process.env.port || 3000
+const DB_NAME = process.env.DB_NAME || ''
 
 app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello world!');
 });
+
+app.get('/data', (req: Request, res: Response) => {
+  const response = db.getAllItems(DB_NAME)
+  res.send(response)
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
---
+++ src/services/data_base.ts
@@ -0,0 +1,46 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const dataBase = process.env.DB_NAME;
+const uri = process.env.DB_URL;
+
+if (!dataBase || !uri) {
+    throw new Error("Environment variables DB_NAME and DB_URL are required.");
+}
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+});
+
+client.connect()
+
+
+class DataBase {
+    constructor() { }
+
+    async connect() {
+        await client.connect();
+    }
+
+    async disconnect() {
+        await client.close();
+    }
+
+    async getAllItems(collectionName: string) {
+        const database = client.db(dataBase);
+        const collection = database.collection(collectionName);
+        const items = await collection.find().toArray();
+        return items;
+    }
+
+}
+
+const db = new DataBase()
+
+export default db;
