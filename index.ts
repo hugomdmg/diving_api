78eda02dec5f87e06eaf97a8c0cac63dbb82e97d
hugomdmg <hugomdmg@gmail.com>
2025-01-01 16:34:34 +0000

    cors

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 import express, { Request, Response } from 'express';
 import db from './src/services/data_base';
+import cors from 'cors'
 
 const app = express();
 const PORT = process.env.port || 3000
 
+app.use(cors())
 app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
