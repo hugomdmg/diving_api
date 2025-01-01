7fb8a74e03809c7740b196dba56f33b18e6680e3
hugomdmg <hugomdmg@gmail.com>
2025-01-01 20:13:00 +0000

    jest

--- jest.config.ts
+++ jest.config.ts
@@ -2,6 +2,7 @@ import { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
   preset: 'ts-jest',
+  collectCoverage: true,
   testEnvironment: 'node',
   testMatch: ['**/*.test.ts'],
   globals: {
@@ -9,6 +10,8 @@ const config: Config.InitialOptions = {
       isolatedModules: true,
     },
   },
+  verbose: true,
+  coverageReporters: ['text-summary', 'lcov', 'cobertura'],
 };
 
 export default config;
